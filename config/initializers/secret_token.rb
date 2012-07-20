314b20d5ceea5c225753077e8a91cbdae4f37ad6
Sarvagya Vaish <sarvagya.vaish7@gmail.com>
2012-07-20 16:43:05 -0400

    created new rails app, got rid of public/index.html

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SnifieWeb::Application.config.secret_token = '92ae93899de6e18736a7f3ca70d159a12ccc2d25856444bce7beca587ef13000fd359628283e59490ec2703d0233b57bc8ed238cab2d000f41de0b8a7834d0cd'

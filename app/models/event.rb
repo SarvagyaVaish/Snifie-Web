dd7771fc6094de1d77845ac9178022838fec6e41
Sarvagya Vaish <sarvagya.vaish7@gmail.com>
2012-07-21 03:07:08 -0400

    configured general carrierwave for images

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,11 @@
 class Event < ActiveRecord::Base
-  attr_accessible :description, :end_time, :key, :key_type, :link, :name, :picture_link, :published, :start_time, :venue_id
+  attr_accessible :description, :end_time, :key, :key_type, :link, :name, :picture_link, :published, :start_time, :venue_id, :image, :remote_image_url
 
   belongs_to :venue
 
   has_many :event_categories
   has_many :categories, :through => :event_categories
+
+  mount_uploader :image, ImageUploader
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120721012534) do
+ActiveRecord::Schema.define(:version => 20120721054738) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(:version => 20120721012534) do
     t.boolean  "published"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+    t.string   "image"
   end
 
   create_table "venues", :force => true do |t|

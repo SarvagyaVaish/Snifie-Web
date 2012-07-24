654a8472ca6060ecd5359f2d51e9b86d43ab76e9
Sarvagya Vaish <sarvagya.vaish7@gmail.com>
2012-07-24 13:43:54 -0400

    added scope sub_categories in categories model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,10 @@
 class Category < ActiveRecord::Base
+
   attr_accessible :name, :parent_id
 
   has_many :event_categories
   has_many :events, :through => :event_categories
+
+  scope :sub_categories, lambda { |id| where("parent_id = ?", id) }
 
 end

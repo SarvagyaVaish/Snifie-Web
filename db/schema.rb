7ab2a3d63da8162200a2875dc9d597abc1d28f43
Sarvagya Vaish <sarvagya.vaish7@gmail.com>
2012-07-24 02:12:22 -0400

    added parent_id column to categories table

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120724034246) do
+ActiveRecord::Schema.define(:version => 20120724061107) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "parent_id"
   end
 
   create_table "event_categories", :force => true do |t|

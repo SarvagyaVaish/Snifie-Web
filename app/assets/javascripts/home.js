74d20f807fd31cd367cced25aab0280b5ce01193
Sarvagya Vaish <sarvagya.vaish7@gmail.com>
2012-07-26 15:03:39 -0400

    added active class to navigation on click

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,17 +1,21 @@
 $('.filter-btn').click(function(){
 	var id = this.id;
 	classToHide = id.replace("-filter", "")
-	console.log(classToHide);
+	console.log(id);
+	
+	$('.sidebar-nav').children().children().removeClass('active')
 
-if (classToHide == 'all'){
-	$('.all-span').css('display', 'block');
-}
-else{
-	$('.all-span').css('display', 'none');
-	$('.' + classToHide + '-span').each(function(){
-		$(this).toggle(); 
-	});
-}
+	if (classToHide == 'all'){
+		$('.all-span').css('display', 'block');
+		$('#all-filter').parent('li').addClass('active');
+	}
+	else{
+		$('.all-span').css('display', 'none');
+		$('.' + classToHide + '-span').each(function(){
+			$(this).toggle();
+			$('#' + classToHide + '-filter').parent('li').addClass('active');
+		});
+	}
 }); 
 
 

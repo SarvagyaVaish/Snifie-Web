a5d1d894aad63854a5b623670c78835845b85001
Sarvagya Vaish <sarvagya.vaish7@gmail.com>
2012-07-26 16:42:53 -0400

    frontend refining

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,36 +1,61 @@
 $(document).ready(function() {
-        $(".dotify").dotdotdot({
-                /*      The HTML to add as ellipsis. */
-                ellipsis                : '... ',
+	$(".dotify").dotdotdot({
+		/*      The HTML to add as ellipsis. */
+		ellipsis                : '... ',
 
-                /*      How to cut off the text/html: 'word'/'letter'/'children' */
-                wrap                    : 'word',
+	/*      How to cut off the text/html: 'word'/'letter'/'children' */
+	wrap                    : 'word',
 
-                /*      jQuery-selector for the element to keep and put after the ellipsis. */
-                after                   : null,
+	/*      jQuery-selector for the element to keep and put after the ellipsis. */
+	after                   : null,
 
-                /*      Whether to update the ellipsis: true/'window' */
-                watch                   : true,
-        
-                /*      Optionally set a max-height, if null, the height will be measured. */
-                height                  : 20,
+	/*      Whether to update the ellipsis: true/'window' */
+	watch                   : true,
 
-                /*      Deviation for the height-option. */
-                tolerance               : 0,
+	/*      Optionally set a max-height, if null, the height will be measured. */
+	height                  : 20,
 
-                /*      Callback function that is fired after the ellipsis is added,
-                        receives two parameters: isTruncated(boolean), orgContent(string). */
-                callback                : function( isTruncated, orgContent ) {},
+	/*      Deviation for the height-option. */
+	tolerance               : 0,
 
-                lastCharacter   : {
+	/*      Callback function that is fired after the ellipsis is added,
+		receives two parameters: isTruncated(boolean), orgContent(string). */
+	callback                : function( isTruncated, orgContent ) {},
 
-                        /*      Remove these characters from the end of the truncated text. */
-                        remove                  : [ ' ', ',', ';', '.', '!', '?' ],
+	lastCharacter   : {
 
-                        /*      Don't add an ellipsis if this array contains 
-                                the last character of the truncated text. */
-                        noEllipsis              : []
-                }
-        });
+		/*      Remove these characters from the end of the truncated text. */
+		remove                  : [ ' ', ',', ';', '.', '!', '?' ],
+
+		/*      Don't add an ellipsis if this array contains 
+			the last character of the truncated text. */
+		noEllipsis              : []
+	}
+	});
+
+
+	$('#all-filter').parent('li').addClass('active');
 });
+
+
+$('.filter-btn').click(function(){
+	var id = this.id;
+	classToHide = id.replace("-filter", "")
+	console.log(id);                
+
+$('.sidebar-nav-links').children('li').removeClass('active')
+
+	if (classToHide == 'all'){
+		$('.all-span').css('display', 'block');
+		$('#all-filter').parent('li').addClass('active');
+	}
+	else{   
+		$('.all-span').css('display', 'none');
+		$('.' + classToHide + '-span').each(function(){
+			$(this).toggle();
+			$('#' + classToHide + '-filter').parent('li').addClass('active');
+		});
+	}       
+});             
+
 
